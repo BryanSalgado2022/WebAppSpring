5e5d16839f5b3aa695524906c6a05541d9635680
BryanSalgadoCode <bsalgado@itstrategys.com>
2024-01-11 15:45:51 -0500

    Clase 16

--- springboot-web/src/main/java/com/curso/springboot/webapp/springbootweb/controllers/UserController.java
+++ springboot-web/src/main/java/com/curso/springboot/webapp/springbootweb/controllers/UserController.java
@@ -5,6 +5,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.curso.springboot.webapp.springbootweb.models.User;
+
 
 @Controller
 public class UserController {
@@ -12,9 +14,11 @@ public class UserController {
     //metodo handler pues manejara datos y regresara algo
     @GetMapping("/details")
     public String details(Model model){
+
+        User user = new User("Bryan", "Salgado");
+
         model.addAttribute("title", "Hola Mundo SpringBoot");
-        model.addAttribute("Name", "Bryan");
-        model.addAttribute("Lastname", "Salgado");
+        model.addAttribute("user", user);
         return "details";
     }
 }
--- springboot-web/src/main/java/com/curso/springboot/webapp/springbootweb/controllers/UserRestController.java
+++ springboot-web/src/main/java/com/curso/springboot/webapp/springbootweb/controllers/UserRestController.java
@@ -9,6 +9,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.curso.springboot.webapp.springbootweb.models.User;
+
 //Se puede dejar tambien solo con el atributo @Controller
 @RestController
 //Se le coloca la ruta de primer nivel
@@ -19,11 +21,12 @@ public class UserRestController {
     @GetMapping("/detailsJson") //El GetMapping y RequestMapping funcionan igual
     //en caso de usar solo el @Controller se debe usar el @RequestBody para regresar el Json
     public Map<String, Object> details(){
+        User user = new User("Bryan", "Salgado");
         Map<String, Object> body = new HashMap<>();
         
         body.put("title", "Hola Mundo SpringBoot");
-        body.put("Name", "Bryan");
-        body.put("Lastname", "Salgado");
+        body.put("user", user);
+
         return body;
     }
 }
